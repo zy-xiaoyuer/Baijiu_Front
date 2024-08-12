1ba30a16044c8b32844f44b578b6baf63bc035fc
hoper-124 <1051544416@qq.com>
2024-08-12 11:17:43 +0800

    Update WinePaintingDetail.vue

--- src/views/winePainting/WinePaintingDetail.vue
+++ src/views/winePainting/WinePaintingDetail.vue
@@ -1,5 +1,112 @@
 <template>
   <div class="about">
-    <h1>酒画细节</h1>
+    <div class="common-layout1">
+      <el-card
+        style="
+          max-width: 14vw;
+          position: relative;
+          width: 18vw;
+          top: 6vw;
+          left: 32vw;
+          margin-left: 2vw;
+          margin-bottom: 2vw;
+        "
+        class="1"
+      >
+        <!-- <template #header>Yummy hamburger</template> -->
+        <img
+          src="https://img.zcool.cn/community/015e215bfb7bf0a80120925252effc.jpg@2o.jpg"
+          style="width: 12vw"
+        />
+      </el-card>
+      <el-container>
+        <el-header
+          style="
+            position: relative;
+            left: 52vw;
+            top: 6vw;
+            width: 12vw;
+            height: 3vw;
+            opacity: 1;
+
+            font-family: Source Han Sans;
+            font-size: 1vw;
+            font-weight: normal;
+            line-height: normal;
+            letter-spacing: 0em;
+
+            font-variation-settings: 'opsz' auto;
+            font-feature-settings: 'kern' on;
+            color: #908D8D;
+          "
+          >出处:XXX</el-header
+        >
+        <el-main
+          style="
+            position: relative;
+            left: 4vw;
+            top: 9vw;
+            width: 12vw;
+            height: 3vw;
+            opacity: 1;
+
+            font-family: Source Han Sans;
+            font-size: 1vw;
+            font-weight: normal;
+            line-height: normal;
+            letter-spacing: 0em;
+
+            font-variation-settings: 'opsz' auto;
+            font-feature-settings: 'kern' on;
+            color: #3D3D3D;
+          "
+          >相关描述</el-main
+        >
+        <el-footer
+          style="
+            position: relative;
+            left: 6vw;
+            top: 10vw;
+            width: 66vw;
+            height: 4vw;
+            opacity: 1;
+
+            font-family: Source Han Sans;
+            font-size: 1vw;
+            font-weight: normal;
+            line-height: normal;
+            letter-spacing: 0em;
+
+            font-variation-settings: 'opsz' auto;
+            font-feature-settings: 'kern' on;
+            color: #908d8d;
+          "
+          >XXX
+</el-footer
+        >
+      </el-container>
+    </div>
   </div>
 </template>
+
+<style lang="less" scoped>
+.about {
+  background-image: url("@/assets/images/background.jpg");
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  width: 96vw;
+  margin-left: 2vw;
+  padding: 0 0 5vw 0;
+  .common-layout1 {
+    position: relative;
+    left: 3vw;
+    top: 2vw;
+    width: 88vw;
+    height: 44vw;
+    border-radius: 1vw;
+    opacity: 1;
+
+    background: #f6f3e5;
+  }
+}
+</style>
