de985796de22366fcba1298b156fae33bc94db7a
hoper-124 <1051544416@qq.com>
2024-08-14 16:29:21 +0800

    Update WinePoetryDetail.vue

--- src/views/winePoetry/WinePoetryDetail.vue
+++ src/views/winePoetry/WinePoetryDetail.vue
@@ -2,67 +2,9 @@
   <div class="about">
     <div class="common-layout1">
       <el-container>
-        <el-header
-          style="
-            position: relative;
-            left: 38vw;
-            top: 3vw;
-            width: 12vw;
-            height: 3vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 1vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-            color: #3d3d3d;
-          "
-          >赠崔秋浦三首 其一</el-header
-        >
-        <el-main
-          style="
-            position: relative;
-            left: 40vw;
-            top: 2vw;
-            width: 12vw;
-            height: 3vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 1vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-            color: #908d8d;
-          "
-          >盛唐 · 李白</el-main
-        >
-        <el-footer
-          style="
-            position: relative;
-            left: 37vw;
-            top: 4vw;
-            width: 15vw;
-            height: 4vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 1vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-            color: #908d8d;
-          "
+        <el-header class="header11">赠崔秋浦三首 其一</el-header>
+        <el-main class="main11">盛唐 · 李白</el-main>
+        <el-footer class="footer11"
           >吾爱崔秋浦，宛然陶令风。 门前五杨柳，井上二梧桐。
           山鸟下厅事，檐花落酒中。 怀君未忍去，惆怅意无穷。</el-footer
         >
@@ -70,73 +12,51 @@
     </div>
     <div class="common-layout2">
       <el-container>
-        <el-header
-          style="
-            position: relative;
-            left: 52vw;
-            top: -24vw;
-            width: 8vw;
-            height: 1vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 0.8vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-            color: #908D8D;
-          "
-          >出处: XXX</el-header
-        >
-        <el-main
-          style="
-            position: relative;
-            left: 4vw;
-            top: -18vw;
-            width: 24vw;
-            height: 12vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 1vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-          "
-          ><h3 style="color: #3D3D3D;">作诗地点:</h3>
-          <h3 style="color: #908D8D;position: relative;left: 6vw;top: -2.7vw;width: 8vw;">安徽省池州市</h3></el-main
-        >
-        <el-footer
-          style="
-            position: relative;
-            left: 4vw;
-            top: -26vw;
-            width: 12vw;
-            height: 12vw;
-            opacity: 1;
-
-            font-family: Source Han Sans;
-            font-size: 1vw;
-            font-weight: normal;
-            line-height: normal;
-            letter-spacing: 0em;
-
-            font-variation-settings: 'opsz' auto;
-            font-feature-settings: 'kern' on;
-          "
+        <el-header class="header21">出处: XXX</el-header>
+        <el-main class="main21"
+          ><h3 style="color: #3d3d3d">作诗地点:</h3>
+          <h3
+            style="
+              color: #908d8d;
+              position: relative;
+              left: 6vw;
+              top: -2.7vw;
+              width: 8vw;
+            "
           >
-          <h3 style="color: #3D3D3D">涉及人物:</h3>
-          <h3 style="color: #3D3D3D; position: relative;left: 6vw;top: -1vw;">李白</h3>
-          <h3 style="color: #908D8D;position: relative;left: 9vw;top: -3.7vw;width: 78vw;">：盛唐 701年1月16日 — 762。唐陇西成纪人，其先人隋末流寓西域，故生于安西都护府所属碎叶城。中宗神龙初，迁居蜀之绵州昌隆县青莲乡，</h3>
-          <h3 style="color: #908D8D;position: relative;left: 6vw;top: -4.5vw;width: 78vw;">又尝寓居山东，故亦称山东人。字太白，号青莲居士。少有逸才，志气宏放，飘然有超世之心。十岁通诗书，被称为“天才英特”。喜纵横术，击剑任侠，轻财重施。青年时离蜀漫游，玄宗天宝初，入长安，经贺知章、吴筠推荐，诏供奉翰林。但政治上不受重视，又受权贵谗毁，仅一年余即离开长安。天宝三载在洛阳结识杜甫。二人于诗坛齐名，并称“李杜”。安史乱起，白为永王李璘府僚，参与平乱。因永王兵败，坐流夜郎，中途遇赦东还，依族人当涂令李阳冰。不久病卒。其诗风雄奇豪放、清新飘逸</h3>
-          </el-footer
+            安徽省池州市
+          </h3></el-main
         >
+        <el-footer class="footer21"
+        >
+          <h3 style="color: #3d3d3d">涉及人物:</h3>
+          <h3 style="color: #3d3d3d; position: relative; left: 6vw; top: -1vw">
+            李白
+          </h3>
+          <h3
+            style="
+              color: #908d8d;
+              position: relative;
+              left: 9vw;
+              top: -3.7vw;
+              width: 78vw;
+            "
+          >
+            ：盛唐 701年1月16日 —
+            762。唐陇西成纪人，其先人隋末流寓西域，故生于安西都护府所属碎叶城。中宗神龙初，迁居蜀之绵州昌隆县青莲乡，
+          </h3>
+          <h3
+            style="
+              color: #908d8d;
+              position: relative;
+              left: 6vw;
+              top: -4.5vw;
+              width: 78vw;
+            "
+          >
+            又尝寓居山东，故亦称山东人。字太白，号青莲居士。少有逸才，志气宏放，飘然有超世之心。十岁通诗书，被称为“天才英特”。喜纵横术，击剑任侠，轻财重施。青年时离蜀漫游，玄宗天宝初，入长安，经贺知章、吴筠推荐，诏供奉翰林。但政治上不受重视，又受权贵谗毁，仅一年余即离开长安。天宝三载在洛阳结识杜甫。二人于诗坛齐名，并称“李杜”。安史乱起，白为永王李璘府僚，参与平乱。因永王兵败，坐流夜郎，中途遇赦东还，依族人当涂令李阳冰。不久病卒。其诗风雄奇豪放、清新飘逸
+          </h3>
+        </el-footer>
       </el-container>
     </div>
   </div>
@@ -161,7 +81,114 @@
     opacity: 1;
 
     background: #f6f3e5;
+    .header11 {
+      position: relative;
+      left: 38vw;
+      top: 3vw;
+      width: 12vw;
+      height: 3vw;
+      opacity: 1;
+
+      font-family: Source Han Sans;
+      font-size: 1vw;
+      font-weight: normal;
+      line-height: normal;
+      letter-spacing: 0em;
+
+      font-variation-settings: "opsz" auto;
+      font-feature-settings: "kern" on;
+      color: #3d3d3d;
+    }
+    .main11 {
+      position: relative;
+      left: 40vw;
+      top: 2vw;
+      width: 12vw;
+      height: 3vw;
+      opacity: 1;
+
+      font-family: Source Han Sans;
+      font-size: 1vw;
+      font-weight: normal;
+      line-height: normal;
+      letter-spacing: 0em;
+
+      font-variation-settings: "opsz" auto;
+      font-feature-settings: "kern" on;
+      color: #908d8d;
+    }
+    .footer11 {
+      position: relative;
+      left: 37vw;
+      top: 4vw;
+      width: 15vw;
+      height: 4vw;
+      opacity: 1;
+
+      font-family: Source Han Sans;
+      font-size: 1vw;
+      font-weight: normal;
+      line-height: normal;
+      letter-spacing: 0em;
+
+      font-variation-settings: "opsz" auto;
+      font-feature-settings: "kern" on;
+      color: #908d8d;
+    }
+  }
+  .common-layout2 {
+    .header21 {
+      position: relative;
+      left: 52vw;
+      top: -24vw;
+      width: 8vw;
+      height: 1vw;
+      opacity: 1;
+
+      font-family: Source Han Sans;
+      font-size: 0.8vw;
+      font-weight: normal;
+      line-height: normal;
+      letter-spacing: 0em;
+
+      font-variation-settings: "opsz" auto;
+      font-feature-settings: "kern" on;
+      color: #908d8d;
+    }
+    .main21 {
+      position: relative;
+      left: 4vw;
+      top: -18vw;
+      width: 24vw;
+      height: 12vw;
+      opacity: 1;
+
+      font-family: Source Han Sans;
+      font-size: 1vw;
+      font-weight: normal;
+      line-height: normal;
+      letter-spacing: 0em;
+
+      font-variation-settings: "opsz" auto;
+      font-feature-settings: "kern" on;
+    }
+    .footer21 {
+      position: relative;
+            left: 4vw;
+            top: -26vw;
+            width: 12vw;
+            height: 12vw;
+            opacity: 1;
+
+            font-family: Source Han Sans;
+            font-size: 1vw;
+            font-weight: normal;
+            line-height: normal;
+            letter-spacing: 0em;
+
+            font-variation-settings: 'opsz' auto;
+            font-feature-settings: 'kern' on;
+    }
   }
 }
 </style>
-
