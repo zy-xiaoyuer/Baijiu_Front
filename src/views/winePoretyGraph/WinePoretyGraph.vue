c757820d50ad8a6000d8b70f815e5cf35b62a9ba
hoper-124 <1051544416@qq.com>
2024-08-18 20:09:49 +0800

    Update WinePoretyGraph.vue

--- src/views/winePoretyGraph/WinePoretyGraph.vue
+++ src/views/winePoretyGraph/WinePoretyGraph.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>酒诗图谱</h1>
   </div>
 </template>
+
+<style>
+.about {
+  position: relative;
+  left: 0vw;
+  top: 0vw;
+  width: 96vw;
+  height: 77vw;
+  opacity: 1;
+  margin-left: 2vw;
+  background-image: url("@/assets/images/background.jpg");
+  background-size: 100% 100%;
+  /* filter: opacity(0.4000000059604645); */
+}
+</style>
